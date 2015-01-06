5e709bedf81d329ae0fc4b3cc24863fa3713162d
farreltr <tracey.farrelly@gmail.com>
2015-01-06 08:47:09 +0000

    First Commit
    Working but needs updated sensitivity on Physics2D.CircleOverlapAll

---
+++ Assets/code/Controller.cs
@@ -0,0 +1,101 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Controller : MonoBehaviour
+{
+
+		private Vector3 screenPoint;
+		private Vector3 offset;
+		//	private Vector2 velocity = new Vector2 (0f, -1f);
+		private float yBound = -7f;
+		private float collisionRadius = 0.1f;
+		private GameControl gameControl;
+		private static string PANDA_BOX = "Panda Box";
+		private static string MONSTER_BOX = "Monster Box";
+
+		void Start ()
+		{
+				gameControl = GameObject.FindObjectOfType<GameControl> ();
+		}
+
+		void OnMouseDown ()
+		{
+				screenPoint = Camera.main.WorldToScreenPoint (gameObject.transform.position);		
+				offset = gameObject.transform.position - Camera.main.ScreenToWorldPoint (new Vector3 (Input.mousePosition.x, Input.mousePosition.y, screenPoint.z));
+		
+		}
+	
+		void OnMouseDrag ()
+		{
+				Vector3 curScreenPoint = new Vector3 (Input.mousePosition.x, Input.mousePosition.y, screenPoint.z);		
+				Vector3 curPosition = Camera.main.ScreenToWorldPoint (curScreenPoint) + offset;
+				transform.position = curPosition;
+		
+		}
+
+		void OnMouseUp ()
+		{
+				float radius = this.transform.localScale.x / 2;
+				Vector2 point = new Vector2 ();
+				point.x = this.transform.position.x + radius;
+				point.y = this.transform.position.y + radius;
+				Collider2D[] colliders = Physics2D.OverlapCircleAll (point, radius, Physics.AllLayers, -Mathf.Infinity, Mathf.Infinity);
+				foreach (Collider2D collider in colliders) {
+						string colliderName = collider.gameObject.name;
+						if (isMonster ()) {
+								if (colliderName == MONSTER_BOX) {
+										gameControl.score++;
+										gameControl.monsterScore++;
+										Destroy (this.gameObject);
+								}
+								if (colliderName == PANDA_BOX) {
+										gameControl.gameOver = true;
+										SetEnabledText ("WrongBoxMonster");		
+								}	
+						} else {
+								if (colliderName == MONSTER_BOX) {
+										gameControl.gameOver = true;
+										SetEnabledText ("WrongBoxPanda");
+								}
+								if (colliderName == PANDA_BOX) {
+										gameControl.score++;
+										gameControl.pandaScore++;
+										Destroy (this.gameObject);
+								}	
+						}
+				}
+		}
+		string GetName ()
+		{
+				return this.name.Replace ("(Clone)", "");
+		}
+
+		bool isMonster ()
+		{
+				return GetName () == "monster";
+		}
+	
+
+		void Update ()
+		{
+				if (transform.position.y < yBound) {
+						if (isMonster ()) {
+								gameControl.gameOver = true;
+								SetEnabledText ("GameOverMonster");
+						} else {
+								gameControl.gameOver = true;
+								SetEnabledText ("GameOverPanda");
+						}
+
+				} else {
+						//			rigidbody2D.velocity = new Vector2 (0f, -1f * (gameControl.score + 1));
+				}
+		}
+
+		void SetEnabledText (string tag)
+		{
+				GameObject.FindGameObjectWithTag (tag).GetComponent<UnityEngine.UI.Text> ().enabled = true;
+
+		}
+	
+}
---
+++ Assets/code/GameControl.cs
@@ -0,0 +1,75 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameControl : MonoBehaviour
+{
+
+		public int score = 0;
+		public Rigidbody2D panda;
+		public Rigidbody2D monster;
+		public float pandaScore = 1f;
+		public float monsterScore = 1f;
+		public bool gameOver = false;
+
+		void Start ()
+		{
+				InvokeRepeating ("SpawnPanda", Random.Range (1, 3), Random.Range (2, 4));
+				InvokeRepeating ("SpawnMonster", Random.Range (1, 3), Random.Range (2, 4));
+		}
+
+		void OnGUI ()
+		{
+				string scoreText = "Score : " + score.ToString ();
+				GUILayout.Label (scoreText);
+				if (gameOver) {
+						CancelInvoke ("SpawnPanda");
+						CancelInvoke ("SpawnMonster");
+						foreach (Controller controller in GameObject.FindObjectsOfType<Controller>()) {
+								Destroy (controller.gameObject);
+						}
+						EnableRestarts ();
+				}
+
+		}
+
+		void SpawnMonster ()
+		{
+				Rigidbody2D instance = Instantiate (monster) as Rigidbody2D;
+				instance.velocity = new Vector2 (0f, -1f * monsterScore);
+				Vector3 position = instance.gameObject.transform.position;
+				position.x = Random.Range (-6, 6);
+				position.y = Random.Range (27, 32);
+				instance.gameObject.transform.position = position;
+		}
+
+		void SpawnPanda ()
+		{
+				Rigidbody2D instance = Instantiate (panda) as Rigidbody2D;
+				instance.velocity = new Vector2 (0f, -1f * pandaScore);
+				Vector3 position = instance.gameObject.transform.position;
+				position.x = Random.Range (-6, 6);
+				position.y = Random.Range (27, 32);
+				instance.gameObject.transform.position = position;
+		}
+
+		public void Restart ()
+		{
+				Application.LoadLevel (Application.loadedLevel);
+		}
+
+		void EnableRestarts ()
+		{
+				GameObject[] restarts = GameObject.FindGameObjectsWithTag ("restart");
+				foreach (GameObject go in restarts) {
+						UnityEngine.UI.Button button = go.GetComponent<UnityEngine.UI.Button> ();
+						UnityEngine.UI.Text text = go.GetComponent<UnityEngine.UI.Text> ();
+						UnityEngine.UI.Image image = go.GetComponent<UnityEngine.UI.Image> ();
+						if (button != null)
+								button.enabled = true;
+						if (text != null)
+								text.enabled = true;
+						if (image != null)
+								image.enabled = true;
+				}
+		}
+}
